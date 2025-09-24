f87341976ec80bbee7c45e47417560db1e2764be
scalprox <152168682+scalprox@users.noreply.github.com>
2025-09-24 22:57:13 +0200

    Add JSDoc comments for utility functions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Sorts an array of train arrivals or departures by their time in ascending order.
+ * @param {TrainArrival[] | TrainDeparture[]} train - The array of train arrival or departure objects to be sorted. Each object must include a `heure` property representing the time as a string in the "HH:mm" format.
+ * @return {TrainArrival[] | TrainDeparture[]} The sorted array of train arrival or departure objects.
+ */
 export function orderTrain(train: TrainArrival[] | TrainDeparture[]): TrainArrival[] | TrainDeparture[] {
     return train.sort((a, b) => {
         const timeToMinutes = (timeStr: string) => {
@@ -9,6 +14,10 @@ export function orderTrain(train: TrainArrival[] | TrainDeparture[]): TrainArriv
     });
 }
 
+/**
+ * Retrieves the current time in a formatted string representing hours and minutes.
+ * @return {string} A string representing the current time in the format "HhMM" where H is the hour and MM is the minute.
+ */
 export function getActualTime(): string {
     const hours = new Date().getHours();
     const minutes = new Date().getMinutes();
