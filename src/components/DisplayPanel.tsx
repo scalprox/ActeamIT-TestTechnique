8dadbb8c22bdcb7f3406fc7747e918251c691091
scalprox <152168682+scalprox@users.noreply.github.com>
2025-09-24 22:12:16 +0200

    Refactor DisplayPanel: added train data integration and styling

--- src/components/DisplayPanel.tsx
+++ src/components/DisplayPanel.tsx
@@ -1,15 +1,28 @@
 import "@/styles/components/DisplayPanel.scss"
+import * as TrainsData from "@/data/trains.json"
+import PanelElement from "@/components/PanelElement.tsx";
+import {orderTrain} from "@/utils";
+import Train from "@/assets/icons/train.tsx";
 
 interface Props {
-    type:"arrival" | "departure"
+    panelType:"arrival" | "departure"
 }
 
-const DisplayPanel = ({type}:Props) => {
+const DisplayPanel = ({panelType}:Props) => {
+
+    const filteredTrains = panelType === "arrival" ? orderTrain(TrainsData.arrivees) : orderTrain(TrainsData.depart)
+
     return (
-        <div className={`container__panel container__panel--${type}`}>
-            <h2>
-                {type === "arrival" ? "Tableaux des arrivées": "Tableaux des départs"}
+        <div className={`container__panel container__panel--${panelType}`}>
+            <h2 className="panel__title">
+                {panelType === "arrival" ? "Prochaines arrivées": "Prochains départs"}
+                <Train/>
             </h2>
+            <ul className="panel__list">
+                {filteredTrains.map((train, index) => (
+                    <PanelElement key={index} train={train}/>
+                ))}
+            </ul>
         </div>
     );
 };
